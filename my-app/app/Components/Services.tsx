351d099420007fbf3db8af317b1aea6aab435952
Muhammad Dawood Iqbal <dawoodjand356@gmail.com>
2025-08-09 20:19:08 +0500

    ongoing animation with gsap

--- my-app/app/Components/Services.tsx
+++ my-app/app/Components/Services.tsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
 import Roboto from "./Roboto";
 import ProjectsCard from "./ProjectsCard";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Projects = () => {
+
+  const parentRef = useRef(null)
+  const paragraphRef = useRef(null)
+
+  useGSAP(() => {
+
+    const split = new SplitText(paragraphRef.current, {type: 'lines'})
+
+    gsap.from(parentRef.current, {
+      y: 200,
+      duration: 1.5,
+      opacity: 0,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: parentRef.current,
+        start: 'top 100%'
+      }
+    })
+    gsap.from('.serhead', {
+      y: 100,
+      duration: 1,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: parentRef.current,
+        start: 'top 90%'
+      }
+    })
+    gsap.from(split.lines, {
+      scrollTrigger: {
+        trigger: paragraphRef.current,
+        start: "top 100%",
+      },
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.1,
+      ease: "power3.out",
+    })
+  }, [])
+
   let urls = [
     "/videos/service1.mp4",
     "/videos/service2.mp4",
-    "/videos/service3.mp4"
+    "/videos/service3.mp4",
   ];
 
   let coverImages = [
@@ -28,19 +74,21 @@ const Projects = () => {
   ];
 
   return (
-    <div className="w-full min-h-screen text-white bg-[#000000] lg:rounded-t-[60px] rounded-t-4xl tracking-tighter md:px-30 lg:px-0 px-6 ">
+    <div ref={parentRef} className="w-full min-h-screen text-white bg-[#000000] lg:rounded-t-[60px] rounded-t-4xl tracking-tighter md:px-30 lg:px-0 px-6 ">
       <div className="flex flex-col items-center lg:py-40 py-20">
         <div className="flex flex-col items-start lg:gap-30 gap-16">
-          <div className="lg:text-[123px] text-5xl flex flex-col items-start lg:leading-24 ">
-            <div className="font-medium">
-              <h1>Our</h1>
+          <div className="lg:text-[123px] text-5xl flex flex-col items-start lg:leading-24 lg:mr-120">
+            <div className="font-medium w-max overflow-hidden">
+              <h1 className="serhead">Our</h1>
             </div>
-            <div className="tracking-wide">
-              <Roboto text="services" />
+            <div className="tracking-wide lg:w-125 w-55 overflow-hidden">
+              <div className="serhead w-full ">
+                <Roboto text="services" />
+              </div>
             </div>
           </div>
           <div className="text-[20px] leading-8 font-thin tracking-wide lg:w-[40%]">
-            <h1>
+            <h1 ref={paragraphRef}>
               From motion design to AI-powered products — we design and build
               interfaces for the future.
             </h1>
