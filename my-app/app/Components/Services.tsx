45ae4fbb838096112ca116f88be8040327bca70e
Muhammad Dawood Iqbal <dawoodjand356@gmail.com>
2025-08-10 20:16:28 +0500

    Animation done for services section

--- my-app/app/Components/Services.tsx
+++ my-app/app/Components/Services.tsx
@@ -9,33 +9,72 @@ import { SplitText } from "gsap/SplitText";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Projects = () => {
+  const parentRef = useRef(null);
+  const paragraphRef = useRef(null);
+  const topTextRef = useRef(null);
+  const bottomTextRef = useRef(null);
+  const buttonRef = useRef(null);
+  const itemsRef = useRef<HTMLDivElement[]>([]);
+  const tl = useRef<gsap.core.Timeline>(null);
 
-  const parentRef = useRef(null)
-  const paragraphRef = useRef(null)
+  const cardRef = (el: HTMLDivElement | null) => {
+    if (el && !itemsRef.current.includes(el)) {
+      itemsRef.current.push(el);
+    }
+  };
 
   useGSAP(() => {
+    gsap.set(topTextRef.current, { y: "0%" });
+    gsap.set(bottomTextRef.current, { y: "100%" });
 
-    const split = new SplitText(paragraphRef.current, {type: 'lines'})
+    tl.current = gsap
+      .timeline({ paused: true })
+      .to(
+        topTextRef.current,
+        {
+          y: "-150%",
+          duration: 0.4,
+          ease: "power2.inOut",
+        },
+        0
+      )
+      .to(
+        bottomTextRef.current,
+        {
+          y: "0%",
+          duration: 0.3,
+          ease: "power2.inOut",
+        },
+        0
+      )
+      .to(buttonRef.current, {
+        scale: 1.03,
+        duration: 0.5,
+        delay: -0.5,
+        ease: "power2.inOut",
+      });
+
+    const split = new SplitText(paragraphRef.current, { type: "lines" });
 
     gsap.from(parentRef.current, {
       y: 200,
       duration: 1.5,
       opacity: 0,
-      ease: 'power2.out',
+      ease: "power2.out",
       scrollTrigger: {
         trigger: parentRef.current,
-        start: 'top 100%'
-      }
-    })
-    gsap.from('.serhead', {
+        start: "top 100%",
+      },
+    });
+    gsap.from(".serhead", {
       y: 100,
       duration: 1,
-      ease: 'power2.out',
+      ease: "power2.out",
       scrollTrigger: {
         trigger: parentRef.current,
-        start: 'top 90%'
-      }
-    })
+        start: "top 90%",
+      },
+    });
     gsap.from(split.lines, {
       scrollTrigger: {
         trigger: paragraphRef.current,
@@ -46,8 +85,37 @@ const Projects = () => {
       duration: 1,
       stagger: 0.1,
       ease: "power3.out",
-    })
-  }, [])
+    });
+    itemsRef.current.forEach((item) => {
+      gsap.from(item, {
+        y: 130,
+        opacity: 0,
+        duration: 1.5,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: item,
+          start: "top 100%",
+        },
+      });
+    });
+    gsap.from(buttonRef.current, {
+      y: 150,
+      duration: 1.5,
+      opacity: 0,
+      ease: "power2.inOut",
+      scrollTrigger: {
+        trigger: ".about",
+        start: "top 30%",
+      },
+    });
+  }, []);
+
+  const handleMouseEnter = () => {
+    tl.current?.play();
+  };
+  const handleMouseLeave = () => {
+    tl.current?.reverse();
+  };
 
   let urls = [
     "/videos/service1.mp4",
@@ -74,7 +142,10 @@ const Projects = () => {
   ];
 
   return (
-    <div ref={parentRef} className="w-full min-h-screen text-white bg-[#000000] lg:rounded-t-[60px] rounded-t-4xl tracking-tighter md:px-30 lg:px-0 px-6 ">
+    <div
+      ref={parentRef}
+      className="w-full min-h-screen text-white bg-[#000000] lg:rounded-t-[60px] rounded-t-4xl tracking-tighter md:px-30 lg:px-0 px-6 "
+    >
       <div className="flex flex-col items-center lg:py-40 py-20">
         <div className="flex flex-col items-start lg:gap-30 gap-16">
           <div className="lg:text-[123px] text-5xl flex flex-col items-start lg:leading-24 lg:mr-120">
@@ -96,7 +167,7 @@ const Projects = () => {
         </div>
         <div className="flex md:flex-row flex-col gap-12 md:gap-0 lg:gap-12 pt-26 md:pt-50 lg:pt-26">
           <div className="flex flex-col items-center gap-14 md:ml-18">
-            <div>
+            <div ref={cardRef}>
               <ProjectsCard
                 height={135}
                 medHeight={135}
@@ -109,7 +180,7 @@ const Projects = () => {
             </div>
           </div>
           <div className="flex flex-col items-center gap-14 md:-mt-30">
-            <div>
+            <div ref={cardRef}>
               <ProjectsCard
                 height={100}
                 medHeight={100}
@@ -120,7 +191,7 @@ const Projects = () => {
                 headingSize={"xl"}
               />
             </div>
-            <div>
+            <div ref={cardRef}>
               <ProjectsCard
                 height={100}
                 medHeight={100}
@@ -134,8 +205,27 @@ const Projects = () => {
           </div>
         </div>
         <div className=" lg:mx-[30%] pt-20">
-          <button className="lg:w-60 lg:h-18 h-20 w-86 border tracking-wide border-white rounded-full text-xl md:text-3xl lg:text-xl">
-            <p>View all services</p>
+          <button
+            ref={buttonRef}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="flex items-center justify-center lg:w-60 lg:h-18 h-20 w-86 relative group border border-white rounded-full overflow-hidden"
+          >
+            <div className="relative h-8 w-full text-xl md:text-3xl lg:text-xl z-10 overflow-hidden">
+              <p
+                ref={topTextRef}
+                className="absolute lg:right-12 md:right-15 right-25 text-white"
+              >
+                View all services
+              </p>
+              <p
+                ref={bottomTextRef}
+                className="absolute lg:right-11 md:right-15 right-24 font-medium text-black"
+              >
+                View all services
+              </p>
+            </div>
+            <div className="absolute left-0 bottom-[-100%] bg-white w-full  h-full rounded-[50%] group-hover:rounded-none group-hover:bottom-0 transition-all duration-450"></div>
           </button>
         </div>
       </div>
--- my-app/app/Components/edit.md
+++ my-app/app/Components/edit.md
@@ -1,7 +1,7 @@
-<button ref={buttonRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="flex items-center justify-center w-max px-4 lg:h-10 md:h-14 h-10 relative group border border-white rounded-full overflow-hidden">
-    <div className="relative h-8 text-center text-base md:text-2xl lg:text-base font-medium  z-10 overflow-hidden">
-      <p ref={topTextRef} className="absolute lg:right-[32%] md:right-40 right-25 text-white">What we do</p>
-      <p ref={bottomTextRef} className="absolute lg:right-[32%] md:right-40 right-25 ">What we do</p>
+<button ref={buttonRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="flex items-center justify-center lg:w-60 lg:h-18 h-20 w-86 relative group border border-white rounded-full overflow-hidden">
+    <div className="relative h-8 text-center text-xl md:text-3xl lg:text-xl z-10 overflow-hidden">
+      <p ref={topTextRef} className="absolute lg:right-[32%] md:right-40 right-25 text-white">View all services</p>
+      <p ref={bottomTextRef} className="absolute lg:right-[32%] md:right-40 right-25 ">View all services</p>
     </div>
     <div className="absolute left-0 bottom-[-100%] bg-white w-full  h-full rounded-[50%] group-hover:rounded-none group-hover:bottom-0 transition-all duration-450"></div>
 </button>
